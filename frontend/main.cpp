1c92e0b068e42b1087b804d9c8cf9cc439ce3e4d
zhouxv <1598284508@qq.com>
2025-08-01 19:50:38 +0800

    删除多余测试代码

--- frontend/main.cpp
+++ frontend/main.cpp
@@ -8,7 +8,6 @@
 #include <ipcl/ipcl.hpp>
 
 #include "fpsi_protocol.h"
-#include "test_all.h"
 
 using namespace osuCrypto;
 int main(int argc, char **argv) {
@@ -16,7 +15,7 @@ int main(int argc, char **argv) {
   cmd.parse(argc, argv);
 
   // 设置日志
-  auto log_level = cmd.getOr<u64>("log", 1);
+  auto log_level = cmd.getOr<u64>("log", 0);
   switch (log_level) {
   case 0:
     spdlog::set_level(spdlog::level::off);
@@ -51,45 +50,6 @@ int main(int argc, char **argv) {
     default:
       throw std::runtime_error("unknown protocol");
     }
-    return 0;
-  }
-
-  if (cmd.isSet("t")) {
-    const u64 protocol_type = cmd.getOr("t", 0);
-
-    switch (protocol_type) {
-    case 1:
-      test_decompose_correction(cmd);
-      break;
-    case 2:
-      test_all_psi_params(cmd);
-      break;
-    case 3:
-      test_if_match_params(cmd);
-      break;
-    case 4:
-      test_paillier();
-      break;
-    case 5:
-      test_bitset();
-      break;
-    case 6:
-      test_u64_random_he(cmd);
-      break;
-    case 7:
-      test_low_bound(cmd);
-      break;
-    case 8:
-      test_batch_pis(cmd);
-      break;
-    case 9:
-      test_paillier_neg();
-      break;
-    default:
-      throw std::runtime_error("unknown protocol");
-    }
-
-    return 0;
   }
 
   return 0;
