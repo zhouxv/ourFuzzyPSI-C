ea1ba903b7025dd25cddba5c7f4ae59155abe201
zhouxv <1598284508@qq.com>
2026-08-28 15:30:13 +0800

    update cmake DCMAKE_BUILD_TYPE=Release

--- shell_build_cmd.sh
+++ shell_build_cmd.sh
@@ -6,6 +6,6 @@ source ./shell_utils.sh
 mkdir -p build && cd build
 
 log "Building the project with CMake and Make..."
-docker_build_style "cmake .." "cmake .."
+docker_build_style "cmake .. -DCMAKE_BUILD_TYPE=Release" "cmake .. -DCMAKE_BUILD_TYPE=Release"
 docker_build_style "make -j$(nproc)" "make -j$(nproc)"
 log "Buidling completed successfully! You can find the built artifacts in the 'build' directory."
